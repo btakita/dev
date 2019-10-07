3c887797abf73c023acbd14d496fcf49661a7d96
Brian Takita <brian.takita@gmail.com>
2019-10-07 02:50:39 -0400

    tmux-dev.sh
    	- dev-sapper-template
    	- sapper-template

--- bin/tmux-dev.sh
+++ bin/tmux-dev.sh
@@ -26,22 +26,4 @@ tmux split-window -v $SHELL
 tmux send-keys 'tig' 'C-m'
 tmux select-pane -t 0
 
-cd $DIR/packages/dev-sapper-template
-tmux new-window $SHELL
-tmux rename-window dev-sapper-template
-tmux split-window -v $SHELL
-tmux select-pane -t 1
-tmux send-keys 'tig' 'C-m'
-tmux select-pane -t 0
-
-cd $DIR/packages/sapper-template
-tmux new-window $SHELL
-tmux rename-window sapper-template
-tmux split-window -h $SHELL
-tmux select-pane -t 0
-tmux send-keys 'yarn run dev' 'C-m'
-tmux split-window -v $SHELL
-tmux send-keys 'tig' 'C-m'
-tmux select-pane -t 1
-
 tmux select-window -t 0
