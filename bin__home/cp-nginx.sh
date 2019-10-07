36f62f8556d86dbbf1389eb1ebe579413416e67d
Brian Takita <brian.takita@gmail.com>
2019-10-07 13:11:57 -0400

    bin__home/cp-nginx.sh: no argument: exit 1

--- bin__home/cp-nginx.sh
+++ bin__home/cp-nginx.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 sudo cp "$1" /etc/nginx/sites-available/
+DIR=$(basename $1)
+if [ -z $DIR ]; then
+  >&2 echo "cp-nginx.sh <path-to-nginx-config-file>"
+  exit 1
+fi
+sudo rm -f /etc/nginx/sites-enabled/$(basename $1)
 sudo ln -s /etc/nginx/sites-available/$(basename $1) /etc/nginx/sites-enabled/
