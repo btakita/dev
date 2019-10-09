609b86c21946a533bd08c179a723807716a8cfe2
Brian Takita <brian.takita@gmail.com>
2019-10-09 10:35:21 -0400

    censible-ghost-theme: title: censible-ghost-theme instead of censible

--- bin__home/dev-censible-ghost-theme.sh
+++ bin__home/dev-censible-ghost-theme.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-fork-termite-tmux.sh -t censible -d ~/work/portfoliome/censible-ghost-theme $SHELL
+fork-termite-tmux.sh -t censible-ghost-theme -d ~/work/portfoliome/censible-ghost-theme $SHELL
