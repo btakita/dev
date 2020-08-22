c7e9b5cb2aec93f8650c9109a35a4196cc0cb23a
Brian Takita <brian.takita@gmail.com>
2020-08-22 04:26:16 -0700

    ↑ dependencies

---
+++ bin__home/dev-menus-web-angular.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+fork-termite-tmux.sh -t menus-web -d ~/work/menus/menus-angular-2-app $SHELL
--- bin__home/dev-menus-web.sh
+++ bin__home/dev-menus-web.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-fork-termite-tmux.sh -t menus-web -d ~/work/menus/menus-angular-2-app $SHELL
+fork-termite-tmux.sh -t menus-web -d ~/work/menus/menus-angular-2-app-sapper $SHELL
