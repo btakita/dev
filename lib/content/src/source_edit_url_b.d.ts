b53dbbaae3a52adb91e7787e7d8345aec99503ca
Brian Takita <brian.takita@gmail.com>
2021-03-29 23:34:24 -0400

    update dependencies

--- lib/content/src/source_edit_url_b.d.ts
+++ lib/content/src/source_edit_url_b.d.ts
@@ -1,1 +1,1 @@
-export declare const source_edit_url_b: import("@ctx-core/object").be_type<import("@ctx-core/store").Writable<string>, object>;
+export declare const source_edit_url_b: import("@ctx-core/object").Be<import("@ctx-core/store").Writable<string>, object>;
