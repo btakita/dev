ee8a9b684608027b6ca12e8233d0bb0208a743da
Brian Takita <brian.takita@gmail.com>
2023-08-28 15:10:54 -0400

    open-dev-tmux: fix: open window sequence
    update dependencies

--- home/bin/open-dev-tmux.sh
+++ home/bin/open-dev-tmux.sh
@@ -2,10 +2,10 @@
 cd ~/work/$1
 if [ -z $TMUX ]; then
   tmux new -s $1
-else
-  tmux new-window $SHELL
-  tmux swap-window -t 0
-  tmux select-window -t 1
-  tmux kill-window
 fi
+tmux new-window $SHELL
 tmux rename-window $1
+tmux move-window -t 0
+tmux select-window -t 1
+tmux kill-window
+tmux select-window -t 0
