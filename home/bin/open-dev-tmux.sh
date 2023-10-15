0ac2a2252433a571aeb0188fea36388497ef0a7d
Brian Takita <brian.takita@gmail.com>
2023-10-14 21:26:10 -0400

    home/bin/open-dev-tmux.sh: when called in a tmux session: fix: window name

--- home/bin/open-dev-tmux.sh
+++ home/bin/open-dev-tmux.sh
@@ -2,10 +2,10 @@
 cd ~/work/$1
 if [ -z $TMUX ]; then
   tmux new -s $1
+  tmux move-window -t 0
 else
-  tmux new-window $SHELL
-  tmux swap-window -t 0
-  tmux select-window -t 1
+  tmux new-window -n $1 $SHELL
+  tmux move-window -t 0
+  tmux select-window -l
   tmux kill-window
 fi
-tmux rename-window $1
